b864ac0fda47c499e614f538dada7f68806140a7
Shivam Gupta <shivamgupta191092005@gmail.com>
2024-05-16 11:07:45 +0530

    import issue solve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login"
-
+import React from "react";
+import { Home,Login,Signup } from "./pages";
 
 function App() {
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./home/Home";
+import Login from "./Login/login";
+import Signup from "./signup/Signup";
+
+export{
+    Home,
+    Login,
+    Signup,
+    
+}
