fcba395d7b0de83fe1dbc735cdc7b146991877f5
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-05-17 10:20:48 +0530

    All section working fine.

--- server/PYfiles/llm.py
+++ server/PYfiles/llm.py
@@ -50,7 +50,7 @@ texts = splitter.split_text(json_data=json_data)
 # ####USING VECTOR STORING#############
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 vector_store=FAISS.from_texts(texts,embeddings)
-vector_store.save_local("faiss_index")
+vector_store.save_local("faiss_index1")
 
 
 
@@ -79,7 +79,7 @@ else:
         raise FileNotFoundError("File not found")
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-prompt="extract the numerical data and give the value along with its relations with other data and textclear. If you are not able to extract the data, just say, 'I am not able to extract the data'"
+prompt="extract the numerical data and give the value along with its relations with other data and reelavent text.Give the response in paragraph format in upto one hundred words s the in a proper structure.   If you are not able to extract the data, just say, 'I am not able to extract the data'"
 
 model=genai.GenerativeModel('gemini-pro-vision')
 response=model.generate_content([pdf_parts[0], prompt])
@@ -105,10 +105,52 @@ vector_store.save_local("faiss_index2")
 
 user_question = "Give summary of this data"
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-new_db=FAISS.load_local("faiss_index2",embeddings)
+
+
+new_db=FAISS.load_local("faiss_index1",embeddings) ###LOADING THE FAISS INDEX of JSON
 docs=new_db.similarity_search(user_question)
-print(docs)
+text1=docs[0].page_content
+# print(text1)
+
+new_db=FAISS.load_local("faiss_index2",embeddings) ###LOADING THE FAISS INDEX of PDF
+docs=new_db.similarity_search(user_question)
+text2=docs[0].page_content
+# print(text2)
+
+text=text1+text2
 
 
 
+
+# Splitting the text into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+chunks=text_splitter.split_text(text)
+# return chunks
+
+# Getting the vector store
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vector_store=FAISS.from_texts(chunks,embeddings)
+vector_store.save_local("faiss_index3")
+
+
+########### LOAD THE FAISS INDEX FOR USER QUESTION ################
+
+new_db=FAISS.load_local("faiss_index3",embeddings) ###LOADING THE FAISS INDEX of Combined JSON and PDF
+docs=new_db.similarity_search(user_question)
+text=docs[0].page_content
+# print(text)
+
+
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+user_question = "what irrigation methods can I perform?"
+
+prompt="You are agriculturist and research expert whose aim is to help the farmers to enhance their productivity by helping them in modernization and sharing scientific aspects.  if you are not able to get any contenxt from the data or not recieved any informatin, then generate your own behalf which reseblence to the query and help the farmers as much as possible. use your own nlp for the purpose. Also avoid any unnecessary information. Give the response in paragraph format in upto one hundred words in a proper structure."
+
+model=genai.GenerativeModel('gemini-pro')
+response=model.generate_content([user_question,text,prompt])
+
+query_answer=response.text
+print(response.text) 
 
