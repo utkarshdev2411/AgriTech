fe1c364d44e65ed85c91100e0dfcd73d9d8d0580
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-05-24 07:27:21 +0530

    refactor: restructure post and comment schemas for improved readability and maintainability

--- server/src/models/post.model.js
+++ server/src/models/post.model.js
@@ -1,17 +1,43 @@
 import mongoose from 'mongoose'
 
 
-const postSchema=new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    },
-    content:{
-        type:String,
-        required:true
+const commentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String, // URL to the uploaded image
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     }
-},{timestamps:true})
+  ],
+  comments: [commentSchema],
+  viewCount: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true })
 
 
-export const postModel=mongoose.model('Post',postSchema)
+export const postModel = mongoose.model('Post', postSchema)
 
